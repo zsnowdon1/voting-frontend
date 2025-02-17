3e78db23f140a9d6e2e6d50aecfb7b80b6a5ef70
zsnowdon <zacksnowdon5@gmail.com>
2025-02-17 14:55:38 -0500

    Refactoring app.module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,23 @@
-import { FormsModule } from '@angular/forms';
-import { CreateSurveyComponent } from './components/create-survey/create-survey.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { RouterModule } from '@angular/router';
 import { CommonModule } from '@angular/common';
+import { HomePageComponent } from './components/home-page/home-page.component';
+import { BrowserModule } from '@angular/platform-browser';
+import { SurveyListComponent } from './components/survey-list/survey-list.component';
+import { CreateSurveyComponent } from './components/create-survey/create-survey.component';
+import { JoinSurveyComponent } from './components/join-survey/join-survey.component';
 
 @NgModule({
-  declarations: [AppComponent, CreateSurveyComponent],
+  declarations: [AppComponent, HomePageComponent, SurveyListComponent, CreateSurveyComponent, JoinSurveyComponent],
   imports: [
-    FormsModule,
     CommonModule,
     RouterModule,
-    ],
-  bootstrap: [CreateSurveyComponent]
+    BrowserModule,
+    FormsModule,
+    ReactiveFormsModule
+  ],
+  bootstrap: [AppComponent]
 })
 export class AppModule { }
