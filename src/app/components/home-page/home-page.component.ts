12dbd373e800ebd6fcc831a3f7f0e44f900f2013
zsnowdon <zacksnowdon5@gmail.com>
2025-02-19 18:15:38 -0500

    Adding survey-list button to home page

--- src/app/components/home-page/home-page.component.ts
+++ src/app/components/home-page/home-page.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
-import { CREATE_SURVEY_ROUTE, JOIN_SURVEY_ROUTE } from '../../constants/routes.const';
+import { CREATE_SURVEY_ROUTE, JOIN_SURVEY_ROUTE, SURVEY_LIST_ROUTE } from '../../constants/routes.const';
 
 @Component({
   selector: 'app-home-page',
@@ -13,15 +13,15 @@ export class HomePageComponent {
   constructor(private router: Router) {}
 
   navToCreate(): void {
-    this.router.navigate([CREATE_SURVEY_ROUTE], {
-      queryParamsHandling: 'preserve'
-    });
+    this.router.navigate([CREATE_SURVEY_ROUTE]);
   }
 
   navToJoin(): void {
-    this.router.navigate([JOIN_SURVEY_ROUTE], {
-      queryParamsHandling: 'preserve'
-    });
+    this.router.navigate([JOIN_SURVEY_ROUTE]);
+  }
+
+  navToList(): void {
+    this.router.navigate([SURVEY_LIST_ROUTE]);
   }
 
 }
